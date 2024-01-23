5ec9e88824b9ed3408f7302baeedfe26d968376b
xiaopya <echo_c120@163.com>
2024-01-23 20:39:19 +0800

    :fire: moven管理工具应用

---
+++ JavaWeb/moven-project/src/main/java/com/itmoven/Main.java
@@ -0,0 +1,7 @@
+package com.itmoven;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ JavaWeb/moven-project/src/test/java/com/itmoventest/DemoTest.java
@@ -0,0 +1,29 @@
+package com.itmoventest;
+
+import org.junit.Test;
+
+/**
+ * @projectName: java学习记录
+ * @package: com.itmoventest
+ * @className: DemoTest
+ * @author: cyl
+ * @description: TODO
+ * @date: 2024/1/23 20:32
+ * @version: 1.0
+ */
+public class DemoTest {
+    @Test
+    public void test1() {
+        System.out.println("测试1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("测试2");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("测试3");
+    }
+}
